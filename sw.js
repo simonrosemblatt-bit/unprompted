5f9c1cf7654253a3e0dfea78901bd5da774641cb
simonrosemblatt-bit <simonrosemblatt@gmail.com>
2026-07-28 19:18:48 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE="unprompted-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",e=>{
+  const req=e.request;
+  if(req.method!=="GET")return;
+  e.respondWith(
+    caches.match(req).then(hit=>hit||fetch(req).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>{try{c.put(req,copy);}catch(_){}});
+      return res;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
